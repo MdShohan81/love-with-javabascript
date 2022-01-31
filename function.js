fb59b682f3c2fa37fd1178414d3ace14aee45a7c
MdShohan81 <mdshohan6563@gmail.com>
2022-01-31 11:00:18 +0600

    function added

---
+++ function.js
@@ -0,0 +1,69 @@
+// function books(){
+//     console.log("ajke ami new boi peyeci");
+// }
+// books();
+
+// function mobile(brand){
+//     console.log("ai mobile ta holo", brand , "company ar");
+    
+// }
+// var brandsName = "Samsung";
+// mobile(brandsName);
+
+// function return//
+
+// function buyBooks(qantity){
+//     console.log("ami boi kinbo ", qantity);
+//     var perPiceBookPrice = 300;
+//     var totalBookPrice = qantity * perPiceBookPrice;
+//     console.log("allah ato takak boi ar dam", totalBookPrice)
+//     return totalBookPrice;
+// }
+// var pice = 30;
+// var books = buyBooks(pice);
+// console.log(books);
+
+// function moneybag(taka){
+   
+//     var khabar = 200;
+//     var totalbill= taka - khabar;
+//     return totalbill;
+// }
+// var money = 400;
+// var showbill = moneybag(money);
+// console.log(showbill);
+
+
+function shohanAdd(num1, num2){
+    var plas = num1 + num2;
+    return plas;
+}
+
+var result = shohanAdd(20, 200);
+console.log(result);
+
+
+function shohanMinus(num1, num2, num3){
+        var minus = num1 - num2 - num3;
+        return minus;
+}
+var result2 = shohanMinus(1000,700,100);
+console.log(result2);
+
+// multiplication 
+
+function shohanMulti(num1, num2, num3,num4){
+    var multi = num1 * num2 * num3 * num4;
+    return multi;
+}
+var result = shohanMulti(3,10,10,10);
+console.log(result)
+
+// divided 
+
+function shohanDivid(num1, num2, num3, num4){
+    var divid = num1 / num2/ num3/ num4;
+    return divid;
+}
+var result = shohanDivid(80,40,2,1);
+console.log(result)
---
+++ switch.js
@@ -0,0 +1,20 @@
+// function getReminder(num1, num2){
+//     var name = num1 % num2;
+//     return name;
+// }
+// const reminder = getReminder(12,2);
+// console.log(reminder);
+
+// function addnumber( num1, num2){
+//     return "results is " + num1 + num2;
+// }
+// var result = addnumber(5,4);
+// console.log(result)
+
+function multiplication(num1, num2){
+    var result = num1 * num2;
+    return result;
+    
+}
+var multi = multiplication(1,3);
+console.log(multi);
